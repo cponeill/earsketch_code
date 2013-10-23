66c26866ffa460b609f86f7d7bdddaae90247e73
cponeill <caseyoneill78@hotmail.com>
2013-10-23 11:44:17 -0700

    Create effects.py

---
+++ effects.py
@@ -0,0 +1,14 @@
+from earsketch import *
+
+init()
+setTempo(120)
+
+#Select the music you want to use
+guitar = GUITAR_FILE_ONE
+bass = BASS_FILE_ONE
+drum_one = DRUM_FILE_ONE
+drum_two = DRUM_FILE_TWO
+
+setEffects(5, Volume, Gain, 12.0)
+
+finish()
