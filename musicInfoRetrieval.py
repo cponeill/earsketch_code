41b675baa4ee2cd0ffcc0a88cdffac767960fdf5
cponeill <caseyoneill78@hotmail.com>
2013-10-26 14:02:16 -0700

    Update musicInfoRetrieval.py

--- musicInfoRetrieval.py
+++ musicInfoRetrieval.py
@@ -5,9 +5,26 @@ from earsketch import *
 init()
 tempo(120)
 
-trackMusic = MUSIC_LOOP_001
+trackMusicOne = MUSIC_LOOP_001
+trackMusicTwo = MUSIC_LOOP_002
 
 start = 1
 end = 3
 
+fitMedia(trackMusicOne, 1, start, end)
+fitMedia(trackMusicTwo, 2, start, end)
 
+analysisMethod = RMS_AMPLITUDE
+one = 0.0625
+RMS_Threshold = 0.35
+
+location = start
+if (location < end):
+  RMS_val = analyzeTrackForTime(1, analysisMethod, location, location + hop)
+  if RMS_val < RMS_Threshold:
+    setEffect(2, VOLUME, GAIN, -60, location, -60, location + one)
+  else:
+    setEffect(2, VOLUME, GAIN, 0, location, 0, location + one)
+  location += one
+  
+finish()
